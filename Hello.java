b9d93063013a1369a7f7931d51b298ade0c83146
liudiddd <609980188@qq.com>
2018-07-02 20:28:08 +0800

    remove notes

--- Hello.java
+++ Hello.java
@@ -2,10 +2,7 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("hello");
-<<<<<<< HEAD
 		System.out.println("asdfasdf");
-=======
 		System.out.println("modify");
->>>>>>> 3d69f11d002914d2bd376ec8831247f4df15fa08
 	}
 }
